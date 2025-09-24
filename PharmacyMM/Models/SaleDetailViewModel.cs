ded6eb79f5df90d359e0483c006c5a7dca5d5724
Abdena <Abdena@ERP-LP-02>
2025-09-24 15:39:22 +0300

    models

---
+++ PharmacyMM/Models/SaleDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PharmacyMM.Models
+{
+    public class SaleDetailViewModel
+    {
+        public int MedicineID { get; set; }
+        public string MedicineName { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal SubTotal => Quantity * UnitPrice;
+    }
+}
---
+++ PharmacyMM/Models/SaleViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PharmacyMM.Models
+{
+    public class SaleViewModel
+    {
+        public int SaleID { get; set; }
+
+        [Display(Name = "Customer")]
+        public int? CustomerID { get; set; }
+        public IEnumerable<Customer> Customers { get; set; }
+
+        [Display(Name = "Sale Date")]
+        public DateTime SaleDate { get; set; } = DateTime.Now;
+
+        public List<SaleDetailViewModel> SaleDetails { get; set; } = new List<SaleDetailViewModel>();
+
+        [Display(Name = "Total Amount")]
+        public decimal TotalAmount { get; set; }
+    }
+}
